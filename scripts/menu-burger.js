8354b3ba05858d7c9948d2aeae4fad7c225aa314
Leo <leovieux.pro@gmail.com>
2024-11-14 15:28:04 +0100

    made a temporary burger menu

---
+++ scripts/menu-burger.js
@@ -0,0 +1,7 @@
+
+  const menuButton = document.getElementById('menuButton');
+  const dropdownMenu = document.getElementById('dropdownMenu');
+  
+  menuButton.addEventListener('click', function() {
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+  });
